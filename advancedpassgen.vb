81f051638f4a4baa1023042e6f56dc9f07cf5e9b
System32 <89716900+System32-0@users.noreply.github.com>
2023-01-30 11:54:33 +1100

    Update advancedpassgen.vb

--- advancedpassgen.vb
+++ advancedpassgen.vb
@@ -1,60 +1,51 @@
-Imports System.Security.Cryptography
+Imports System
+Imports System.Text
 
-Module Module1
+Module AdvancedPasswordGenerator
+
+    Dim names As String() = {"John", "Jane", "Tom", "Sophie", "Michael", "Emma", "William", "Olivia", "James", "Isabella"}
 
     Sub Main()
-        Console.WriteLine("System32's Password Generator")
-        Console.WriteLine("------------------")
+        Console.WriteLine("System32's Advanced Password Generator")
+        Console.WriteLine("---------------------------")
+        Console.Write("Enter password length: ")
+        Dim length As Integer = Convert.ToInt32(Console.ReadLine())
+        Console.Write("Include lowercase letters (y/n): ")
+        Dim useLowercase As Boolean = (Console.ReadLine().ToLower() = "y")
+        Console.Write("Include uppercase letters (y/n): ")
+        Dim useUppercase As Boolean = (Console.ReadLine().ToLower() = "y")
+        Console.Write("Include numbers (y/n): ")
+        Dim useNumbers As Boolean = (Console.ReadLine().ToLower() = "y")
+        Console.Write("Include special characters (y/n): ")
+        Dim useSpecial As Boolean = (Console.ReadLine().ToLower() = "y")
 
-        Console.WriteLine("Enter password length:")
-        Dim length As Integer = Integer.Parse(Console.ReadLine())
+        Dim password As String = GeneratePasswordWithRandomName(length, useLowercase, useUppercase, useNumbers, useSpecial)
 
-        Console.WriteLine("Include lowercase characters? (Y/N)")
-        Dim useLowercase As Boolean = (Console.ReadLine().ToUpper() = "Y")
+        Console.WriteLine("Your password is: " + password)
+    End Sub
 
-        Console.WriteLine("Include uppercase characters? (Y/N)")
-        Dim useUppercase As Boolean = (Console.ReadLine().ToUpper() = "Y")
+    Function GeneratePassword(length As Integer, useLowercase As Boolean, useUppercase As Boolean, useNumbers As Boolean, useSpecial As Boolean) As String
+        Dim validChars As String = ""
+        If useLowercase Then validChars += "abcdefghijklmnopqrstuvwxyz"
+        If useUppercase Then validChars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        If useNumbers Then validChars += "0123456789"
+        If useSpecial Then validChars += "!@#$%^&*?"
 
-        Console.WriteLine("Include numbers? (Y/N)")
-        Dim useNumbers As Boolean = (Console.ReadLine().ToUpper() = "Y")
+        Dim password As String = ""
+        Dim random As New Random()
+        For i As Integer = 0 To length - 1
+            password += validChars(random.Next(0, validChars.Length))
+        Next
+        Return password
+    End Function
 
-        Console.WriteLine("Include special characters? (Y/N)")
-        Dim useSpecial As Boolean = (Console.ReadLine().ToUpper() = "Y")
-
-        Console.WriteLine("Generating password...")
+    Function GeneratePasswordWithRandomName(length As Integer, useLowercase As Boolean, useUppercase As Boolean, useNumbers As Boolean, useSpecial As Boolean) As String
+        Dim random As New Random()
+        Dim randomName As String = names(random.Next(0, names.Length))
 
         Dim password As String = GeneratePassword(length, useLowercase, useUppercase, useNumbers, useSpecial)
 
-        Console.WriteLine("Password: " + password)
-        Console.ReadLine()
-    End Sub
-
-    Private Function GeneratePassword(length As Integer, useLowercase As Boolean, useUppercase As Boolean, useNumbers As Boolean, useSpecial As Boolean) As String
-        Dim chars As String = ""
-        If useLowercase Then
-            chars += "abcdefghijklmnopqrstuvwxyz"
-        End If
-        If useUppercase Then
-            chars += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        End If
-        If useNumbers Then
-            chars += "0123456789"
-        End If
-        If useSpecial Then
-            chars += "!@#$%^&*?"
-        End If
-
-        Dim password As New Char(length - 1) {}
-        Dim data(0) As Byte
-
-        Using crypto As New RNGCryptoServiceProvider()
-            For i As Integer = 0 To length - 1
-                crypto.GetBytes(data)
-                password(i) = chars(data(0) Mod chars.Length)
-            Next
-        End Using
-
-        Return New String(password)
+        Return randomName + password
     End Function
 
 End Module
